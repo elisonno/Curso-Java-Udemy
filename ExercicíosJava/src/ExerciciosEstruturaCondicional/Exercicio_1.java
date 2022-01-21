51a06ce9a640a59ed9fc33bdb53f8bea17a19b99
elisonno <elisonno997@gmail.com>
2022-01-21 14:51:03 -0300

    Adicionando exercício 2

--- ExercicíosJava/src/ExerciciosEstruturaCondicional/Exercicio_1.java
+++ ExercicíosJava/src/ExerciciosEstruturaCondicional/Exercicio_1.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Exercicio_1 {
 
 	public static void main(String[] args) {
+		/*Fazer um programa para ler um número inteiro, e 
+		 * depois dizer se este número é negativo ou não.*/		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Digite um número");
 		int a = sc.nextInt();
