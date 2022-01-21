87e3c4bf7db67f1e07e9ea77b70ee551469c736b
elisonno <elisonno997@gmail.com>
2022-01-21 16:38:19 -0300

    Adicionando exercício 4

---
+++ ExercicíosJava/src/ExerciciosEstruturaCondicional/Exercicio_4.java
@@ -0,0 +1,33 @@
+package ExerciciosEstruturaCondicional;
+
+import java.util.Scanner;
+
+public class Exercicio_4 {
+
+	public static void main(String[] args) {
+		/*Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo,
+		sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração 
+		mínima de 1 hora e máxima de 24 horas.*/ 
+		int horaInicio, horaFim, soma;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Insira a hora que o jogo começou");
+		horaInicio = sc.nextInt();
+		System.out.println("Insira a hora que o jogo terminou");
+		horaFim = sc.nextInt();
+		if(horaInicio == horaFim) {
+			soma = 24;
+		}
+		else if(horaInicio > horaFim) {
+			soma = 24 - horaInicio + horaFim;
+		}
+		else {
+			soma = horaFim - horaInicio;
+		}
+		System.out.printf("O jogo durou %d horas", soma);
+		
+		sc.close();
+		
+		
+	}
+
+}
